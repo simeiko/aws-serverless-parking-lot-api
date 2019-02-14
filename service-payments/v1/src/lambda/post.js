2f1a7e0c8b104197019ba331ccb1c5daec835c8d
Valentyn Simeiko <valentyn@simeiko.pro>
2019-02-13 21:49:46 -0500

    - Added AWS SDK to both microservices. - Added SNS publish to the [POST] /payments route.

--- service-payments/v1/src/lambda/post.js
+++ service-payments/v1/src/lambda/post.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
+const AWS = require('aws-sdk');
 const { promisify } = require('util');
+const sns = new AWS.SNS();
 
 const verifyJWT = promisify(jwt.verify);
 const Utils = require('../utils/Utils');
@@ -7,12 +9,14 @@ const Database = require('../utils/Database/Database');
 
 /**
  * POST: process pay_token and complete the payment.
+ * @todo before creating a ticket, ensure that ticket was not paid.
  */
 exports.handler = async event => {
     if (!event.headers.pay_token || !event.headers.credit_card) {
         return Utils.error({ message: 'Headers `pay_token` and `credit_card` must be present.' });
     }
 
+    // Validate credit card number. Let's pretend that we actually take money...
     if (!/^\d{12,19}$/.test(event.headers.credit_card)) {
         return Utils.error({ message: 'Invalid credit card.' });
     }
@@ -35,6 +39,12 @@ exports.handler = async event => {
     const db = new Database();
     await db.create.payment(ticket.id, ticket.cost_of_stay);
     db.close();
+
+    // Notify other microservices about successful payment.
+    sns.publish({
+        Message: `Successful payment for ticket ID ${ticket.id}`,
+        TopicArn: `arn:aws:sns:us-east-1:${process.env.AWS_ACCOUNT_ID}:closeTicket`,
+    });
 
     return Utils.success({ message: 'Payment was completed successfully.' });
 };
