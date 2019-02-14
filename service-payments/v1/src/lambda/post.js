e1444084cfe5b0fd4e90ab472e5f2ddfc8348333
Valentyn Simeiko <valentyn@simeiko.pro>
2019-02-13 21:13:13 -0500

    - Completed [POST] /payments route. - Added Create.payment() method.

--- service-payments/v1/src/lambda/post.js
+++ service-payments/v1/src/lambda/post.js
@@ -1,3 +1,36 @@
-exports.handler = async event => {
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
+const verifyJWT = promisify(jwt.verify);
+const Utils = require('../utils/Utils');
+const Database = require('../utils/Database/Database');
+
+
+exports.handler = async event => {
+    if (!event.headers.pay_token) {
+        return Utils.error({ message: 'Header `pay_token` must be present.' });
+    }
+
+    /**
+     * @type {{id: number, cost_of_stay: number}}
+     */
+    let ticket;
+
+    try {
+        await verifyJWT(event.headers.pay_token, process.env.JWT_SECRET_KEY);
+        // eslint-disable-next-line prefer-destructuring
+        ticket = jwt.decode(event.headers.pay_token, process.env.JWT_SECRET_KEY).ticket;
+
+        if (!ticket) {
+            throw new Error('Ticket info is not present.');
+        }
+    } catch (e) {
+        return Utils.error({ message: 'Unauthorized.' });
+    }
+
+    const db = new Database();
+    await db.create.payment(ticket.id, ticket.cost_of_stay);
+    db.close();
+
+    return Utils.success({ message: 'Payment was completed successfully.' });
 };
--- service-payments/v1/src/utils/Database/crud/Create.js
+++ service-payments/v1/src/utils/Database/crud/Create.js
@@ -9,6 +9,23 @@ class Create {
     constructor(knex) {
         this.query = knex;
     }
+
+    /**
+     * Make payment.
+     *
+     * @public
+     * @version 1.0
+     * @param   {number} ticketId   Ticket ID
+     * @param   {number} amount     Pay amount
+     * @returns {Promise<number>} Payment ID
+     */
+    async payment(ticketId, amount) {
+        const [paymentID] = await this.query('payments')
+            .returning('id')
+            .insert({ ticket_id: ticketId, amount });
+
+        return paymentID;
+    }
 }
 
 module.exports = Create;
